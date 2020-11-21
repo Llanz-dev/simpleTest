b37b0c7c32e2d58e8521fc362e9532d14997c934
llanz-dev <llanzkennethluy@gmail.com>
2020-11-21 18:11:36 +0800

    commit

--- app.js
+++ app.js
@@ -6,21 +6,37 @@ let buttonTwo = document.getElementById("buttonTwo");
 let question = document.getElementById("question");
 let h2 = document.querySelector("h2");
 let passContainer = document.getElementById("pass-container");
+let allInputs = document.querySelectorAll("input");
+let p1 = document.createElement("p");
+let text1;
+let p2 = document.createElement("p");
+let text2;
+let p3 = document.createElement("p");
+let text3;
+let p4 = document.createElement("p");
+let text4;
+let p5 = document.createElement("p");
+let text5;
 window.addEventListener("load", () => {
-    passContainer.style.display = "none";
-    document.getElementById("buttonOne").addEventListener("click", function() {
+    passContainer.style.display = "none";    
+    buttonOne.addEventListener("click", function() {
         passContainer.style.display = "none";
     })
 })
 
+function checkCursor(input){
+    if(input.disabled = true){
+        input.className = "notCursor";
+    } else {
+        input.className = "txtCursor";
+    }
+}
 // number 1
 let input1 = document.getElementById("answer1");
 input1.addEventListener("keyup", function(e) {
     if(e.keyCode === 13){
         e.preventDefault();
         const answer1 = input1.value;
-        const p1 = document.createElement("p");
-        let text1;
         if(answer1 == 4){
             text1 = document.createTextNode(answer1 + " is correct");
             input1.style.border = "1px solid green";
@@ -30,12 +46,14 @@ input1.addEventListener("keyup", function(e) {
             text1 = document.createTextNode(answer1 + " is wrong");
             input1.style.border = "1px solid red";
             p1.style.color = "red";
-            countMistake++;
+            countMistake++;            
         }
         p1.appendChild(text1);
         input1.disabled = true;
+        checkCursor(input1);
         if(state == "start"){
             input2.disabled = false;
+            input2.className = "txtCursor";
             state = "not";
         }
         document.getElementById("answerDiv1").appendChild(p1);
@@ -45,13 +63,11 @@ input1.addEventListener("keyup", function(e) {
 // number 2
 let input2 = document.getElementById("answer2");
 input2.disabled = true;
-
+input2.className = "notCursor";
 input2.addEventListener("keyup", function(e) {
     if(e.keyCode === 13){
         e.preventDefault();
-        const answer2 = input2.value;
-        const p2 = document.createElement("p");
-        let text2;
+        const answer2 = input2.value;        
         if(answer2 == 30){
             text2 = document.createTextNode(answer2 + " is correct");
             input2.style.border = "1px solid green";
@@ -65,8 +81,10 @@ input2.addEventListener("keyup", function(e) {
         }
         p2.appendChild(text2);
         input2.disabled = true;
+        checkCursor(input2);
         if(state === "not"){
             input3.disabled = false;
+            input3.className = "txtCursor";
             state = "start";
         }
         document.getElementById("answerDiv2").appendChild(p2);
@@ -76,12 +94,11 @@ input2.addEventListener("keyup", function(e) {
 // number 3
 let input3 = document.getElementById("answer3");
 input3.disabled = true;
+input3.className = "notCursor";
 input3.addEventListener("keyup", function(e) {
     if(e.keyCode === 13){
         e.preventDefault();
         const answer3 = input3.value;
-        const p3 = document.createElement("p");
-        let text3;
         if(answer3 == 11){
             text3 = document.createTextNode(answer3 + " is correct");
             input3.style.border = "1px solid green";
@@ -95,8 +112,10 @@ input3.addEventListener("keyup", function(e) {
         }
         p3.appendChild(text3);
         input3.disabled = true;
+        checkCursor(input3);
         if(state === "start"){
             input4.disabled = false;
+            input4.className = "txtCursor";
             state = "not";
         }
         document.getElementById("answerDiv3").appendChild(p3);
@@ -106,12 +125,11 @@ input3.addEventListener("keyup", function(e) {
 // number 4
 let input4 = document.getElementById("answer4");
 input4.disabled = true;
+input4.className = "notCursor";
 input4.addEventListener("keyup", function(e) {
     if(e.keyCode === 13){
         e.preventDefault();
-        const answer4 = input4.value;
-        const p4 = document.createElement("p");
-        let text4;
+        const answer4 = input4.value;        
         if(answer4 == 4){
             text4 = document.createTextNode(answer4 + " is correct");
             input4.style.border = "1px solid green";
@@ -125,8 +143,10 @@ input4.addEventListener("keyup", function(e) {
         }
         p4.appendChild(text4);
         input4.disabled = true;
+        checkCursor(input4);
         if(state === "not"){
             input5.disabled = false;
+            input5.className = "txtCursor";
             state = "start";
         }
         document.getElementById("answerDiv4").appendChild(p4);
@@ -136,12 +156,11 @@ input4.addEventListener("keyup", function(e) {
 // number 5
 let input5 = document.getElementById("answer5");
 input5.disabled = true;
+input5.className = "notCursor";
 input5.addEventListener("keyup", function(e) {
     if(e.keyCode === 13){
         e.preventDefault();
-        const answer5 = input5.value;
-        const p5 = document.createElement("p");
-        let text5;
+        let answer5 = input5.value;
         if(answer5 == 1){
             text5 = document.createTextNode(answer5 + " is correct");
             input5.style.border = "1px solid green";
@@ -155,8 +174,13 @@ input5.addEventListener("keyup", function(e) {
         }
         p5.appendChild(text5);
         input5.disabled = true;
+        checkCursor(input5);
+        if(state === "not"){
+            input5.disabled = false;    
+            input5.className = "txtCursor";
+            state = "start";
+        }
         document.getElementById("answerDiv5").appendChild(p5);
-        if(state === "start"){
             const correct = "Number of correct: " + countCorrect;
             const mistake = "Number of mistake: " + countMistake;
             const correctCount = document.createElement("h1");
@@ -171,18 +195,51 @@ input5.addEventListener("keyup", function(e) {
             if(countCorrect > 2 ){
                 buttonOne.innerText = "Back";
                 buttonTwo.innerText = "Continue";
-                h2.innerHTML = "You passed the test!";
+                h2.innerHTML = "You passed the test!";  
             } else {
+                countCorrect = 0;
+                countMistake = 0;
                 buttonOne.innerText = "Back";
                 buttonTwo.innerText = "Restart";
+                h2.style.color = "red";
                 h2.innerHTML = "You failed the test";
                 question.innerText = "Do you want to restart?";
                 buttonTwo.addEventListener("click", () => {
-                    passContainer.style.display = "none";
-                    input1.disabled = false;
+                   
+                    if(countCorrect < 2){
+                        h2.style.color = "rgb(0, 255, 0)";
+                    }
+                    passContainer.style.display = "none";                    
+                    other();
                     answer1.value = "";
+                    p1.removeChild(text1);                    
+                    document.getElementById("answerDiv1").removeChild(p1);                    
+                    input1.disabled = false;
+                    input1.className = "txtCursor";
+                    input1.style.border = "1px solid black";                    
+                    correctCount.remove();
+                    mistakeCount.remove();                    
                 })
             }
-        }
     }
 })
+function other(){
+    answer2.value = "";
+    answer2.style.border = "1px solid black";
+    p2.removeChild(text2);
+    document.getElementById("answerDiv2").removeChild(p2);
+
+    answer3.value = "";
+    answer3.style.border = "1px solid black";
+    p3.removeChild(text3);
+    document.getElementById("answerDiv3").removeChild(p3);
+
+    answer4.value = "";
+    answer4.style.border = "1px solid black";
+    p4.removeChild(text4);
+    document.getElementById("answerDiv4").removeChild(p4);
+
+    answer5.value = "";
+    answer5.style.border = "1px solid black";
+    p5.removeChild(text5);
+}

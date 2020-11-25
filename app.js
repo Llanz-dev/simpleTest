936b20e0854c06ba2dcf1f8243c9cd9690a433b5
llanz-dev <llanzkennethluy@gmail.com>
2020-11-25 21:03:16 +0800

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let click = 0;
 let countMistake = 0;
 let countCorrect = 0;
 let countNone = 0;
-let state = "start";
+let state;
 let buttonBack = document.getElementById("buttonBack");
 let buttonRetake = document.getElementById("buttonRetake");
 let buttonContinue = document.getElementById("buttonContinue");
@@ -59,15 +59,6 @@ function correctAnswer(text, input, p, userAnswer){
     countCorrect++;
 }        
 
-
-// Function if the answer is empty
-function emptyAnswer(answerUser, input){
-    if(answerUser == ""){
-        input.disabled = false;
-        alert("It must be filled out");
-    } 
-}
-
 // Function if the answer is wrong
 function wrongAnswer(text, input, p, userAnswer){
     text = document.createTextNode(userAnswer + " is wrong");
@@ -88,7 +79,22 @@ function inputsJudgement(answerUser, keyAnswer, text, input, p){
     }
 }
 
-// To run the logic of the test
+// Function if the answer is empty
+function emptyAnswer(userAnswer, answerKey, text, input, p){
+    // Function if the user did not answer the test
+    if(input.value == ""){
+        input.disabled = false;
+        alert("It must be filled out");
+        state = "not";        
+    }
+    // Function if the user did not answer the test
+    else {
+        inputsJudgement(userAnswer, answerKey, text, input, p);
+        state = "start";
+    }
+}
+
+// To run all the function of the test
 testLogic();
 
 // All the test logic functions
@@ -101,18 +107,11 @@ function testLogic(){
             userAnswer = "answer" + number;
             userAnswer = input1.value;
             answerKey = 4;            
-            if(input1.value == ""){
-                alert("13212");
-                state = "not";
-            } else {
-                inputsJudgement(userAnswer, answerKey, text1, input1, p1);
-                state = "start";
-            }
+            emptyAnswer(userAnswer, answerKey, text1, input1, p1);            
             document.getElementById("answerDiv1").appendChild(p1);            
             if(state === "start"){                
                 input2.disabled = false;
-                input2.className = "txtCursor";
-                state = "not";
+                input2.className = "txtCursor";                
             }            
         }
     })
@@ -126,18 +125,11 @@ function testLogic(){
             number = 2;
             userAnswer = "answer" + number;
             userAnswer = input2.value;        
-            answerKey = 30;        
-            if(input2.value == ""){
-                alert("12312");
-                state = "start";
-            } else {
-                inputsJudgement(userAnswer, answerKey, text2, input2, p2);        
-                state = "not";
-            }
-            if(state === "not"){
+            answerKey = 30;               
+            emptyAnswer(userAnswer, answerKey, text2, input2, p2);
+            if(state === "start"){
                 input3.disabled = false;
-                input3.className = "txtCursor";
-                state = "start";
+                input3.className = "txtCursor";                
             }
             document.getElementById("answerDiv2").appendChild(p2);
         }
@@ -152,18 +144,11 @@ function testLogic(){
             number = 3;
             userAnswer = "answer" + number;
             userAnswer = input3.value;
-            answerKey = 11;
-            if(input3.value == ""){
-                alert("12312");
-                state = "not";
-            } else {
-                inputsJudgement(userAnswer, answerKey, text3, input3, p3);        
-                state = "start";
-            }
+            answerKey = 11;            
+            emptyAnswer(userAnswer, answerKey, text3, input3, p3);
             if(state === "start"){
                 input4.disabled = false;
-                input4.className = "txtCursor";
-                state = "not";
+                input4.className = "txtCursor";                
             }
             document.getElementById("answerDiv3").appendChild(p3);
         }
@@ -178,18 +163,11 @@ function testLogic(){
             number = 4;
             userAnswer = "answer" + number;
             userAnswer = input4.value;        
-            answerKey = 4;
-            if(input4.value == ""){
-                alert("12312");
-                state = "start";
-            } else {
-                inputsJudgement(userAnswer, answerKey, text4, input4, p4);
-                state = "not";
-            }
-            if(state === "not"){
+            answerKey = 4;            
+            emptyAnswer(userAnswer, answerKey, text4, input4, p4);
+            if(state === "start"){
                 input5.disabled = false;
-                input5.className = "txtCursor";
-                state = "start";
+                input5.className = "txtCursor";                
             }
             document.getElementById("answerDiv4").appendChild(p4);
         }
@@ -204,20 +182,13 @@ function testLogic(){
             number = 5;
             userAnswer = "answer" + number;
             userAnswer = input5.value;
-            answerKey = 1;
-            if(input5.value == ""){
-                alert("12312");
-                state = "start";
-            } else {
-                inputsJudgement(userAnswer, answerKey, text5, input5, p5);
-                state = "not";
-            }
-            if(state === "not"){
+            answerKey = 1;            
+            emptyAnswer(userAnswer, answerKey, text5, input5, p5);
+            if(state === "start"){
                 input5.disabled = true;    
                 input5.className = "notCursor";
-                state = "start";
-                // After the test
                 document.getElementById("answerDiv5").appendChild(p5);
+                // After the test
                 const correct = "Number of correct: " + countCorrect;
                 const mistake = "Number of mistake: " + countMistake;
                 const noAnswer = "Number of blank answer: " + countNone;
@@ -241,7 +212,7 @@ function testLogic(){
                     question.innerText = "Do you want to continue?";   
                 } 
                 // Showing the output if the user failed the test
-                else if (countCorrect < 3){
+                else {
                     h2.classList.add("red");
                     h2.innerHTML = "You failed the test";
                     question.innerText = "Do you want to retake?";
